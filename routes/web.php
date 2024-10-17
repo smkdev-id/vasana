30e586252c7a1079c3bf4155eb17288a8afd8dee
sapategu <gultomterampil19@gmail.com>
2024-10-18 04:38:32 +0700

    feat: add Admin & User panel provider

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\OllamaController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/user');
 });
-
-Route::get('/ollama', function () {
-    return view('ollama.form'); // For displaying the input form
-});
-Route::post('/ollama/send', [OllamaController::class, 'sendToOllama'])->name('ollama.send');
