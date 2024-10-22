ba8edf2002907b97ffe9c6d53753e5e362d9425b
sapategu <gultomterampil19@gmail.com>
2024-10-23 01:38:32 +0700

    feat: update entrypoint & apache permission

--- deployment/docker-entrypoint.sh
+++ deployment/docker-entrypoint.sh
@@ -1,45 +1,18 @@
-#!/bin/sh
-
+#!/bin/bash
 set -e
 
-LATEST_VERSION=$(cat "/usr/src/smkdev/version")
-
-if [ -f "version" ]
-then
-    CURRENT_VERSION=$(cat "version")
-    if [ $CURRENT_VERSION -ne $LATEST_VERSION ]
-    then
-        echo "Updating the app... ($CURRENT_VERSION => $LATEST_VERSION)"
-        rm -rf ./*
-        cp -r /usr/src/smkdev/* .
-    fi
-else
-    echo "Copying the app... (Version: $LATEST_VERSION)"
-    cp -r /usr/src/smkdev/* .
-fi
-
-if [ ! -f "node_modules/.package-lock.json" ]
-then
-    echo "Running npm install"
-    npm install --quiet
-    echo "Running npm run build"
-    npm run build --quiet
-fi
-
-if [ ! -f "vendor/autoload.php" ]
-then
-    echo "Running composer install"
-    composer install --quiet --no-interaction --no-dev
-fi
-
-if [ ! -f ".env" ]
-then
+# Check if .env file exists, if not create it and generate an APP_KEY
+if [ ! -f ".env" ]; then
+    echo "Creating .env file..."
     echo APP_ENV=prod > .env
     echo APP_DEBUG=false >> .env
     echo APP_KEY= >> .env
     php artisan key:generate --quiet --no-interaction
+    echo ".env file created and APP_KEY generated."
 fi
 
-chown -R www-data:www-data .
+# Set ownership of the files to www-data user
+chown -R www-data:www-data /var/www/html
 
+# Execute the CMD passed as arguments
 exec "$@"
