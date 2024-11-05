abba984921df333aa341259c603ae967caaf1924
sapategu <gultomterampil19@gmail.com>
2024-11-05 12:22:45 +0700

    feat: add official SMKDEV links

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -6,8 +6,11 @@ use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages;
+use App\Filament\Pages\Settings;
+use Filament\Navigation\MenuItem;
 use Filament\Panel;
 use Filament\Navigation\NavigationGroup;
+use Filament\Navigation\NavigationItem;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
 use Filament\Widgets;
@@ -28,20 +31,40 @@ class AppPanelProvider extends PanelProvider
             ->id('app')
             ->path('app')
             ->login()
+            ->registration()
+            // ->passwordReset()
+            ->emailVerification()
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->brandLogo('https://smkdev.storage.googleapis.com/wp/SMKDEV-Logo-Long-150x38.png')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
                 Pages\Dashboard::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
-            ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+            ->sidebarCollapsibleOnDesktop()
+            ->userMenuItems([
+                MenuItem::make()
+                    ->label('Settings')
+                    ->url(fn (): string => Settings::getUrl())
+                    ->icon('heroicon-o-cog-6-tooth'),
+                // ...
             ])
-            ->darkMode(false)
+            ->navigationItems([
+                NavigationItem::make('SMKDEV Website')
+                    ->url('https://www.smk.dev/', shouldOpenInNewTab: true)
+                    ->icon('heroicon-m-user-group')
+                    ->group('Official')
+                    ->sort(3),
+                NavigationItem::make('LMS SMKDEV')
+                    ->url('https://lms.smk.dev/', shouldOpenInNewTab: true)
+                    ->icon('heroicon-s-book-open')
+                    ->group('Official')
+                    ->sort(3),
+            ])
+            // ->darkMode(false)
             // ->viteTheme('resources/css/filament/app/theme.css')
             ->middleware([
                 EncryptCookies::class,
