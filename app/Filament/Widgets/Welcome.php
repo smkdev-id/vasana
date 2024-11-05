fd27dd6177188af034916e3b2da18e08a668c086
sapategu <gultomterampil19@gmail.com>
2024-11-05 12:20:30 +0700

    feat: initializae widget

---
+++ app/Filament/Widgets/Welcome.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+
+class Welcome extends Widget
+{
+    protected static string $view = 'filament.widgets.welcome';
+    protected static ?int $sort = 0;
+}
