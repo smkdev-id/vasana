bce191c6fba6cbe3c6b48137555600cd14a34576
sapategu <gultomterampil19@gmail.com>
2024-11-05 12:21:57 +0700

    feat: Models for Resources

---
+++ app/Models/Category.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Coding;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    //
+}
---
+++ app/Models/Stack.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Talent;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Stack extends Model
+{
+    //
+}
