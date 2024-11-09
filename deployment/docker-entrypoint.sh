81710f48ec936b136938f077e2f91b4164784911
sapategu <gultomterampil19@gmail.com>
2024-11-09 09:47:13 +0700

    fix: move to docker-entrypoint.sh

--- deployment/docker-entrypoint.sh
+++ deployment/docker-entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+# Check if autoload.php file exists, if not run composer install
+if [ ! -f "vendor/autoload.php" ]
+then
+    echo "Running 'composer install'"
+    composer install --quiet --no-interaction --no-dev --prefer-dist --optimize-autoloader
+fi
+
 # Check if .env file exists, if not create it and generate an APP_KEY
 if [ ! -f ".env" ]; then
     echo "Creating .env file..."
@@ -30,6 +37,7 @@ php artisan filament:optimize --quiet --no-interaction
 
 # Set ownership of the files to www-data user
 chown -R www-data:www-data /var/www/html
+# chmod -R 755 /var/www/html/storage /var/www/html/bootstrap/cache
 
 # Execute the CMD passed as arguments
 exec "$@"
