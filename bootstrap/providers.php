3d72746d1d79e7f548288f60de341b706b251767
sapategu <adalgisoabercio@gmail.com>
2024-12-01 21:38:40 +0700

    feat: wip

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,6 +4,4 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\AppPanelProvider::class,
-    App\Providers\Filament\ChallengePanelProvider::class,
-    App\Providers\Filament\TalentPanelProvider::class,
 ];
